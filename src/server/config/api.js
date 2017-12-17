ac4bdf3ee7d7bd737d4754def584742ebb3f5ff1
cheese <xiaomo0329@gmail.com>
2017-12-17 11:55:47 +0800

    1

--- src/server/config/api.js
+++ src/server/config/api.js
@@ -1,6 +1,6 @@
 var testController = require('../controller/testController')
 
 module.exports = function (app) {
-    app.get('/api/test', testController.get);
-    app.post('/api/test', testController.post);
+    // app.get('/api/test', testController.get);
+    // app.post('/api/test', testController.post);
 }
